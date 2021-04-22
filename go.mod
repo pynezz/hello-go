d6c7ecc0ca793a0098d15dd02719ba9be68d2329
Kevin <kevin.ka_96@hotmail.com>
2021-04-22 15:02:37 +0200

    web scraper testing done

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/pynezz
 
 go 1.16
 
-require github.com/gocolly/colly v1.2.0
+require (
+	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.6 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20210421230115-4e50805a0758 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
--- scraper.go
+++ scraper.go
@@ -1,14 +1,25 @@
 package main
 
 //
+//
 //			 Go web scraper
 //				 0.0.1
 //
+//
+
+// Usage: go run scraper.go
+// curl -s 127.0.0.1:7171/search/<URL>
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gocolly/colly"
 )
+
+var startTime time.Time
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	log.Println("Ping")
@@ -16,9 +27,62 @@ func ping(w http.ResponseWriter, r *http.Request) {
 }
 
 func scraper() {
+
 	addr := ":7171"
+
+	// Endpoints
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/search", getData)
 
 	log.Println("listening on", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+// Get link from website
+
+func getData(w http.ResponseWriter, r *http.Request) {
+
+	startTime = time.Now()
+
+	// Verify that the URL param exists
+	URL := r.URL.Query().Get("url")
+	if URL == "" {
+		log.Println("Missing URL..")
+		return
+	}
+	log.Println("visiting", URL)
+
+	// Create a collector that will handle the data collection from HTML
+	c := colly.NewCollector()
+	var response []string
+
+	// OnHTML function allows using a callback function
+	// when a certain HTML-tag i reached, call the anonymous
+	// function, and add the info to the slice 'response'
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if link != "" {
+			response = append(response, link)
+		}
+	})
+
+	// Visit URL
+	c.Visit(URL)
+
+	// Response slice to JSON
+	b, err := json.Marshal(response)
+	if err != nil {
+		log.Println("failed to serialize response:", err)
+		return
+	}
+
+	// Add a header and write the body to the endpoint
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(b)
+
+	t := time.Now()
+	elapsed := t.Sub(startTime)
+
+	log.Printf("Scraped in %v", elapsed)
+
 }
