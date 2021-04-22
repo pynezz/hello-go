2973dec8ce2552088c8ea87b364fd73f8ff0dc0a
Kevin <kevin.ka_96@hotmail.com>
2021-04-22 12:16:11 +0200

    return / := / add / parameters

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
+
 	hello "github.com/pynezz/hello_world"
 )
 
+var age int
+
 func main() {
+	RunPackageHello_Go()
+}
+
+func PrintString() {
+	age = 24
+	fmt.Printf("\nMy age: %d \n", age)
+	added := AddTwoInts(2, 2)
+	fmt.Printf("2 + 2 = %v", added)
+}
+
+func AddTwoInts(i int, j int) int {
+	add := func(i int, j int) int {
+		k := i + j
+		return k
+	}
+	return add(i, j)
+}
+
+func RunPackageHello_Go() {
 	h := hello.Hello{
 		Hello: "Hey",
 	}
 	h.Hello_Go()
+
+	PrintString()
 }
